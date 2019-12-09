b713f4a91e6aaa7190fffdccb49215538bbeb342
grese <johngrese@yahoo.com>
2019-12-09 14:34:22 -0800

    Polyarc symbolic arrays

--- src/cmu/symbolic/PolyArcTest.java
+++ src/cmu/symbolic/PolyArcTest.java
@@ -9,22 +9,27 @@ import java.util.List;
 
 public class PolyArcTest {
     
-    public static PolyArc create(double lat, double lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg) {
-        LatLon location = LatLon.fromDegrees(lat, lon);
+    public static PolyArc create(double[] lats, double[] lons, double radius, double leftAzimuthDeg, double rightAzimuthDeg) {      
+        
         Angle leftAzimuth = Angle.fromDegrees(leftAzimuthDeg);
         Angle rightAzimuth = Angle.fromDegrees(rightAzimuthDeg);
-        List<LatLon> locations = Arrays.asList(location);
+        List<LatLon> locations = Arrays.asList();
+        int locsCount = Math.min(lats.length, lons.length);
+
+        for (int i=0; i < locsCount; i++) {
+            locations.add(LatLon.fromDegrees(lats[i], lons[i]));
+        }
         
         return new PolyArc(locations, radius, leftAzimuth, rightAzimuth);
     }
     
-    public static void setRadius(double lat, double lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg, double newRadius) {
+    public static void setRadius(double[] lat, double[] lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg, double newRadius) {
         PolyArc polyarc = create(lat, lon, radius, leftAzimuthDeg, rightAzimuthDeg);
         
         polyarc.setRadius(newRadius);
     }
     
-    public static void setAzimuths(double lat, double lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg, double newLeftAzimuth, double newRightAzimuth) {
+    public static void setAzimuths(double[] lat, double[] lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg, double newLeftAzimuth, double newRightAzimuth) {
         PolyArc polyarc = create(lat, lon, radius, leftAzimuthDeg, rightAzimuthDeg);
         Angle leftAzimuth = Angle.fromDegrees(newLeftAzimuth);
         Angle rightAzimuth = Angle.fromDegrees(newRightAzimuth);
@@ -32,7 +37,7 @@ public class PolyArcTest {
         polyarc.setAzimuths(leftAzimuth, rightAzimuth);
     }
     
-    public static void move(double lat, double lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg, double newLat, double newLon, double newElev) {
+    public static void move(double[] lat, double[] lon, double radius, double leftAzimuthDeg, double rightAzimuthDeg, double newLat, double newLon, double newElev) {
         PolyArc polyArc = create(lat, lon, radius, leftAzimuthDeg, rightAzimuthDeg);
         Position position = Position.fromDegrees(newLat, newLon, newElev);
         
@@ -43,10 +48,10 @@ public class PolyArcTest {
         // disable GUI
         System.setProperty("java.awt.headless", "true");
 
-        create(1.0, 1.0, 1.0, 1.0, 1.0);
-        setRadius(1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
-        setAzimuths(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
-        move(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8);
+        create(new double[] {}, new double[] {}, 1.0, 1.0, 1.0);
+        setRadius(new double[] {}, new double[] {}, 1.0, 1.0, 1.0, 1.0);
+        setAzimuths(new double[] {}, new double[] {}, 1.0, 1.0, 1.0, 1.0, 1.0);
+        move(new double[] {}, new double[] {}, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8);
     }
 
 }
